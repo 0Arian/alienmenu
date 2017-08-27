4bde503f08bc1a62138aa97c95c055ec73e5fe53
arianmoslem <amoslem678@gmail.com>
2017-08-27 17:26:39 -0400

    fix: Icon location fixed

--- src/menubar.js
+++ src/menubar.js
@@ -1,5 +1,6 @@
 const menubar = require('menubar')
 const serve = require('serve')
+const path = require('path')
 
 const port = process.env.PORT ? (process.env.PORT - 100) : 7160
 
@@ -13,7 +14,9 @@ var mb = menubar({
     index: `http://localhost:${port}`,
     width: 750,
     height: 1100,
-    icon: 'IconTemplate.png'
+    icon: path.join(__dirname, 'IconTemplate.png')
 })
+
+console.log(__dirname);
 
 mb.on('ready', () => console.log('App Started'))
