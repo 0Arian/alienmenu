5398fee46e2ac8e064df84098785d4625c8df5bc
arianmoslem <amoslem678@gmail.com>
2017-08-27 11:46:28 -0400

    final: Finalizing build to be packaged for electron

--- src/menubar.js
+++ src/menubar.js
@@ -1,7 +1,15 @@
-var menubar = require('menubar')
+const menubar = require('menubar')
+const serve = require('serve')
+
+const port = process.env.PORT ? (process.env.PORT - 100) : 7160
+
+const server = serve('./build', {
+  single: ["../build"],
+  port: port,
+})
 
 var mb = menubar({
-    index: 'http://localhost:3000'
+    index: `http://localhost:${port}`
 })
 
 mb.on('ready', () => console.log('app is ready'))
