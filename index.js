9237e57693ade5aae0b313ee5d67ac82cc014025
airrbear <amoslem678@gmail.com>
2017-09-07 19:02:10 -0400

    Fix: removed serve and used a hosted website for index page
    - Client does not self-host react build anymore

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const menubar = require('menubar')
-const serve = require('serve')
 const path = require('path')
 const fixPath = require('fix-path');
 
 fixPath()
 
-const port = process.env.PORT ? (process.env.PORT - 100) : 5000
-
-const server = serve('./build', {
-  single: ["./build"],
-  port: port,
-  silent: true
-})
-
 var mb = menubar({
-    index: `http://localhost:${port}`,
+    index: 'http://wiry-flock.surge.sh/',
     width: 750,
     height: 1100,
     icon: path.join(__dirname, 'src', 'IconTemplate.png')
